a3793c7a00aa2a4c4eb04f81f50cbdf598e377b5
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-02-18 19:22:31 +0530

    Update CorsConfig.java

--- src/main/java/com/Jadhav/Contest_tracker/config/CorsConfig.java
+++ src/main/java/com/Jadhav/Contest_tracker/config/CorsConfig.java
@@ -14,7 +14,12 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://code-alarm-contest.vercel.app"));
+        configuration.setAllowedOrigins(
+    Arrays.asList(
+        "https://code-alarm-contest.vercel.app",
+        "http://localhost:5173"
+    )
+);
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);

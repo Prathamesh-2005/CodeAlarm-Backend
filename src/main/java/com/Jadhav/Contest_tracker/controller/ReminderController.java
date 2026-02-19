a91a72799b88e365d8288605bad7bd188e48c12b
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-02-19 23:07:47 +0530

    Add new CORS origin for production

--- src/main/java/com/Jadhav/Contest_tracker/controller/ReminderController.java
+++ src/main/java/com/Jadhav/Contest_tracker/controller/ReminderController.java
@@ -19,7 +19,8 @@ import java.util.Map;
 @RequestMapping("/api/reminders")
 @CrossOrigin(origins = {
     "https://code-alarm-contest.vercel.app",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://www.codealarm.tech/"
 })
 public class ReminderController {
 
